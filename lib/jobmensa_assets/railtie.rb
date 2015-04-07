ca067f97704c80155472a02283987f251b22fb78
Andreas Riemer <andreas.riemer@gmail.com>
2015-04-07 15:24:15 +0200

    Fixes initializer for rails

--- lib/jobmensa_assets/railtie.rb
+++ lib/jobmensa_assets/railtie.rb
@@ -1,7 +1,7 @@
 module JobmensaAssets
   class Railtie < Rails::Railtie
 
-    initializer 'jobmensa_assets.configure'
+    initializer 'jobmensa_assets.configure' do
       require 'jobmensa_assets/config'
     end
 
